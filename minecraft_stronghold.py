72ec915119e1fc4e066306f421a3c78ea3e4fab5
Skye Chen <skyechen28@gmail.com>
2024-04-14 04:07:05 -0400

    locate stronghold

--- minecraft_stronghold.py
+++ minecraft_stronghold.py
@@ -1,24 +1,37 @@
-from math import tan, pi
+from math import sin, cos, tan, pi
+from typing import Tuple
 
-### Inputs
-t1, t2 = -44.2, -41  # Yaw
-x1,x2=89.693,170.906 # x coords
-z1,z2=27.925,-34.545 # z coords
+def locateStronghold(x1: float, z1: float, yaw1: float, x2: float, z2: float, yaw2: float) -> Tuple[float]:
 
-# Minecraft inverts the z axis compared to normal cartesian coords
-y1,y2=-z1,-z2
+    # Define division by zero threshold
+    dbz_threshold = 0.01
 
-# Convert Minecraft's yaw to Cartesian slope
-def slope(theta):
-    x = -theta -90
-    return tan(x*pi/180)
-m1,m2=slope(t1), slope(t2)
+    # Function for converting minecraft yaw to cartesian angles
+    def convertYawMcToCart(yaw_mc: float) -> float:
+        return (-yaw_mc - 90) * pi/180
+    
+    # Convert MC coords to Cartesian
+    z1, z2 = -z1, -z2 
+    theta1, theta2 = convertYawMcToCart(yaw1), convertYawMcToCart(yaw2)
 
-# Answer
-x = (m1 * x1 - m2*x2 + y2-y1) / (m1-m2)
-y=-(m1*(x-x1)+y1) # Must invert the z coords back from cartesian to mc coords
-print(x,y)
+    # Calculate the rise and run of both lines
+    dx1, dx2 = cos(theta1), cos(theta2)
+    dz1, dz2 = sin(theta1), sin(theta2)
 
-# Sanity check (should be identical to answer)
-y=-(m2*(x-x2)+y2)
-print(x,y)
+    # Calculate the x coord
+    x = ((dx1 * dx2 * (z2-z1)) - (dz2 * dx1 * x2) + (dz1 * dx2 * x1)) / (sin(theta1 - theta2))
+
+    # Calculate the z coord
+    if dx1 < dbz_threshold:
+        z = tan(theta2) * (x - x2) + z2
+    else:
+        z = tan(theta1) * (x - x1) + z1
+
+    # Convert from Cartesian back into MC coords
+    z = -z
+
+    # Round the numbers
+    x = round(x)
+    z = round(z)
+
+    return (x, z)
